7e6d40ed755d562d359fa417807d93207b0ec36e
yxpmatrix <2426574593@qq.com>
2019-03-08 15:40:44 +0800

    repair some bug

--- src/main/java/com/yxp/yzjxc/service/sys/EmployeeService.java
+++ src/main/java/com/yxp/yzjxc/service/sys/EmployeeService.java
@@ -1,5 +1,7 @@
 package com.yxp.yzjxc.service.sys;
-import com.yxp.yzjxc.entity.dic;
+
+import com.yxp.yzjxc.entity.Employee;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -10,5 +12,5 @@ public interface EmployeeService {
     public boolean insertEmployee(Employee employee);
     public boolean updateEmployee(Employee employee);
     public Page<Employee> employeeList(String employeeId, String employeeName, Date birthdayBegin, Date birthdayEnd ,
-                                        int sexId, int eduId , String pageNo,String pageSize);
+                                       int sexId, int eduId , String pageNo, String pageSize);
 }
--- src/main/java/com/yxp/yzjxc/serviceimpl/sys/EmployeeServiceImpl.java
+++ src/main/java/com/yxp/yzjxc/serviceimpl/sys/EmployeeServiceImpl.java
@@ -1,7 +1,11 @@
 package com.yxp.yzjxc.serviceimpl.sys;
 
+import com.yxp.yzjxc.entity.Employee;
+import com.yxp.yzjxc.repo.sys.IemployeeRepo;
+import com.yxp.yzjxc.service.sys.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -23,7 +27,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public Page<Employee> employeeList(String employeeId, String employeeName, Date birthdayBegin,
-                                       Date birthdayEnd, int sexId, int eduId, String pageNo,String pageSize) {
+                                       Date birthdayEnd, int sexId, int eduId, String pageNo, String pageSize) {
        Pageable pageable= new PageRequest(Integer.parseInt(pageNo),Integer.parseInt(pageSize));
         Criteria criteria = Criteria.where("employeeId").regex("/*." + employeeId +".*/");
 
